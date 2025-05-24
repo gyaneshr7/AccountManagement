cda4b0efaa0957914381a3b566dff30a93e6e18c
GYANESH <gyaneshr7@gmail.com>
2025-05-24 11:28:31 +0530

    Deletion functionality implemented in a seperate file

---
+++ AccountManagementSystem/AccountDeletion.java
@@ -0,0 +1,13 @@
+package AccountManagementSystem;
+
+/** @author hp */
+public class AccountDeletion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
